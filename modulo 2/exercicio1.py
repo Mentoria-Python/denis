a3a1b7ce3000c0527298bfe5aabbafa928c03d79
Denis <denischarocha@gmail.com>
2023-03-06 13:31:35 -0300

    colocando o exercicio 1

---
+++ modulo 2/exercicio1.py
@@ -0,0 +1,9 @@
+unidade_sigla = input('Coloque a sigla: ')
+unidade_codigo = input('Coloque o codigo: ')
+unidade_classe_consumidora = input('Coloque a classe consumidora: ')
+unidade_conexao = input('Coloque a conexão: ')
+unidade_condutor_tensao1 = input('Coloque o condutor tensao1: ')
+unidade_condutor_tensao2 = input('Coloque o condutor tensao2: ')
+
+print(unidade_sigla, '-',unidade_codigo, '-', unidade_classe_consumidora, '-', unidade_conexao, '-',
+      unidade_conexao, '-', unidade_condutor_tensao1, '-', unidade_condutor_tensao2)
